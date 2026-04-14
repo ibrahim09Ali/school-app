2ace80f873e9df81fa59df5b4a91d95e64e9eeaf
ibrahim09Ali <ibrahimtaweel@outlook.com>
2026-04-14 06:19:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'school-app-v1';
+const assets = [
+  './',
+  './index.html',
+  './data.json',
+  './manifest.json'
+];
+
+// تثبيت ملفات التطبيق في الذاكرة المؤقتة
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// تشغيل التطبيق من الذاكرة حتى لو لا يوجد إنترنت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
