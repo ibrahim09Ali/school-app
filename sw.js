611c8665d70626c869b7fdc0eaa1324bd0439df0
ibrahim09Ali <ibrahimtaweel@outlook.com>
2026-04-14 12:19:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
-const cacheName = 'school-app-v1.1';
+const cacheName = 'school-app-v1.2'; // غير هذا الرقم مع كل تحديث قادم
 const assets = [
   './',
   './index.html',
   './data.json',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
-// تثبيت ملفات التطبيق في الذاكرة المؤقتة
+// 1. التثبيت (تحميل الملفات الجديدة)
 self.addEventListener('install', e => {
+  // إجبار التحديث الجديد على تخطي طابور الانتظار
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assets);
@@ -15,7 +18,19 @@ self.addEventListener('install', e => {
   );
 });
 
-// تشغيل التطبيق من الذاكرة حتى لو لا يوجد إنترنت
+// 2. التفعيل (عامل النظافة: مسح أي إصدار قديم من الذاكرة)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key)) // حذف الذاكرة القديمة
+      );
+    })
+  );
+});
+
+// 3. التشغيل (جلب الملفات من الذاكرة أو الإنترنت)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
